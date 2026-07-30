05b2c89034c8034943a8fe248eccf7df7fa80622
Ganesh <ganesh@code.com>
2026-07-30 12:42:48 +0530

    Initial Django BuddyConnect project

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
